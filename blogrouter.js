37c16ad0fa00d5929dd72439121caaf08095c9fe
Harkirpa <kirpa704@gmail.com>
2023-10-13 22:13:30 +0530

    node2

--- blogrouter.js
+++ blogrouter.js
@@ -6,6 +6,6 @@ blogrouter.get("/",(req,res)=>{
             "what is express":"Express.js is a small framework that works on top of Node.js web server functionality to simplify its APIs and add helpful new features. It makes it easier to organize your application’s functionality with middleware and routing."
            
           
-    })
+    });
 })
 module.exports=blogrouter
